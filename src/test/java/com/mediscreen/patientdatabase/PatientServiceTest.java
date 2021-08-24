8ce351f0bea21656fc15be728122c37c4f9271d8
Jason <jasondubourg@gmail.com>
2021-08-24 22:22:36 +0200

    Modification du test du service

--- src/test/java/com/mediscreen/patientdatabase/PatientServiceTest.java
+++ src/test/java/com/mediscreen/patientdatabase/PatientServiceTest.java
@@ -9,11 +9,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+
 
 import java.util.ArrayList;
 import java.util.List;
